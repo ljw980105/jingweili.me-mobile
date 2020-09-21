db4e82a2db471c561eadfd190126c0011d062987
Jing Wei Li <ljw9801055@gmail.com>
2020-09-20 21:02:26 -0400

    Initial Commit

---
+++ jingweili.me-mobile/jingweili_me_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  jingweili_me_mobileApp.swift
+//  jingweili.me-mobile
+//
+//  Created by Jing Wei Li on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct jingweili_me_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
